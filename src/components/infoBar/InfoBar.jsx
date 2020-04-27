a2efbd546df6d7d2faa5c1d04f17412ead8a0a64
Devendra Singh <devsinghindra@gmail.com>
2020-04-27 19:41:10 +0530

    added infobar and its styles

---
+++ src/components/infoBar/InfoBar.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+import onlineIcon from "../../icons/onlineIcon.png";
+import closeIcon from "../../icons/closeIcon.png";
+
+import "./InfoBar.css";
+
+function InfoBar({ room }) {
+  return (
+    <div className="infoBar">
+      <div className="leftInnerContainer">
+        <img className="onlineIcon" src={onlineIcon} alt="online" />
+        <h3>{room}</h3>
+      </div>
+      <div className="rightInnerContainer">
+        <a href="/">
+          <img className="closeIcon" src={onlineIcon} alt="close" />
+        </a>
+      </div>
+    </div>
+  );
+}
+
+export default InfoBar;

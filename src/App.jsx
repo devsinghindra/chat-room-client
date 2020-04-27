c1a2b9f6f7ae34a009b203af5b74a6268f9fd919
Devendra Singh <devsinghindra@gmail.com>
2020-04-27 22:03:52 +0530

    added root route and chat route

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Join from "./components/join/Join";
+import Chat from "./components/chat/Chat";
 
 function App() {
   return (
-    <div>
-      <h1>Hello</h1>
-    </div>
+    <Router>
+      <Route path="/" exact component={Join} />
+      <Route path="/chat" component={Chat} />
+    </Router>
   );
 }
 
